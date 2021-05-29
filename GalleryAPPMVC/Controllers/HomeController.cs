3af8ef130b1fc323eccee2b683cd219a8956e122
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-05-29 19:55:25 +0530

    Add project files.

---
+++ GalleryAPPMVC/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using GalleryAPPMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GalleryAPPMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult StudentView(Person person)
+        {
+
+            string strcon = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            SqlConnection connect = new SqlConnection(strcon);
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("GalleryLogin", connect);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Name", person.Name);
+            cmd.Parameters.AddWithValue("@UserName", person.UserName);
+            cmd.Parameters.AddWithValue("@Password", person.Password);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            String dataSetUserName;
+            String dataSetPassword;
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+
+                dataSetUserName = ds.Tables[0].Rows[0]["UserName"].ToString();
+                dataSetPassword = ds.Tables[0].Rows[0]["Password"].ToString();
+
+                if (dataSetUserName == person.UserName && dataSetPassword == person.Password)
+                {
+
+                    return View("Gallery");
+                }
+                else
+                {
+                    person.Message = "Invalid Login";
+                }
+            }
+            else
+            {
+                person.Message = "Invalid UserName and Password";
+            }
+
+            return View("Gallery");
+        }
+    
+        
+       
+        
+    }
+}
---
+++ GalleryAPPMVC/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace GalleryAPPMVC.Models
+{
+    public class Person
+    {
+        
+       
+        public String  Name { get; set; }
+
+        public String UserName { get; set; }
+
+        public String Password { get; set; }
+
+        public Image images { get; set; }
+
+        public String Message { get; set; }
+
+
+
+
+    }
+}
